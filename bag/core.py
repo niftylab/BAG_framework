f0568e399af92c6f25a72dab7af31f57371085e3
Jaeduk Han <jdhan@hanyang.ac.kr>
2020-06-17 15:45:44 +0900

    Disabling XBase 	modified:   bag/core.py

--- bag/core.py
+++ bag/core.py
@@ -16,7 +16,7 @@ from pathlib import Path
 from .interface import ZMQDealer
 from .interface.database import DbAccess
 from .design import ModuleDB, SchInstance
-from .layout.routing import RoutingGrid
+# from .layout.routing import RoutingGrid  # disabled XBase to minimize package dependencies
 from .layout.template import TemplateDB
 from .layout.core import DummyTechInfo
 from .io import read_file, sim_data, read_yaml_env
